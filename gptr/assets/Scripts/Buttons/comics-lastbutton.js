8b77147aec0ff297e035ff0e1816f0b4fb9a2420
Rob <robert.korolevskiy@deuscraft.com>
2017-07-02 14:21:58 +0700

    fishy prefabs, hovers

---
+++ gptr/assets/Scripts/Buttons/comics-lastbutton.js
@@ -0,0 +1,16 @@
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        popupNode: cc.Node,
+    },
+
+    // use this for initialization
+    onLoad: function () {
+
+    },
+
+    onClick: function () {
+      this.popupNode.active = true;  
+    },
+});
---
+++ gptr/assets/Scripts/Buttons/comics-nextbutton.js
@@ -0,0 +1,16 @@
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+
+    },
+
+    // use this for initialization
+    onLoad: function () {
+
+    },
+
+    onClick: function () {
+      cc.director.loadScene("Main-screen");  
+    },
+});
